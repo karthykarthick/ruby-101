ff946f40c492a85ab71868ac856bbb527a01280c
karthykarthick <karthylock@gmail.com>
2020-08-21 16:42:47 +0530

    update

---
+++ Ruby-Programs/square and cube/square_cube.rb
@@ -0,0 +1,7 @@
+puts "-----------square and cube------------"
+puts "Ebter the value"
+value = gets.to_i
+square = value*value
+cube = value*value*value
+puts "The square value is #{square}"
+puts "The cube value is #{cube}"

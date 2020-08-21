123652052211dea3dd75d3ec4a114ed6242646c0
karthykarthick <karthylock@gmail.com>
2020-08-21 15:19:31 +0530

    Add subraction program in ruby

---
+++ Ruby-Programs/Subraction/subraction.rb
@@ -0,0 +1,7 @@
+puts "Add two values"
+puts "Enter the value for a to subract"
+a = gets.to_i # to get the value from the user and we can store the value in variable a
+puts "Enter the value for b to subract"
+b = gets.to_i # to get the value from the user and we can store the value in variable b
+c = a-b # Here we simply subracts two number and store it in the new variable c
+puts"The subracted value is #{c}"

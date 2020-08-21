e0454dc6aaf54cdf1d2bfa30a0e3106b9879ad91
karthykarthick <karthylock@gmail.com>
2020-08-21 16:19:37 +0530

    add area of the triangle program in ruby

---
+++ Ruby-Programs/Area-of-Triangle/area_triangle.rb
@@ -0,0 +1,8 @@
+puts "----------Area of Triangle------------"
+# Area of the triangle formula is 0.5*breath*height
+puts "Enter the breath value"
+breath = gets.to_i
+puts "enter the height value"
+height = gets.to_i
+triangle  = 0.5*breath*height
+puts "The area of the triangle is #{triangle}"

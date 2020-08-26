9b505f2e5dfa5bc23ebd82f99fc967518e9e0c3d
karthykarthick <karthylock@gmail.com>
2020-08-27 00:52:35 +0530

    add to check whether a number is positive or negative

---
+++ Ruby-Programs/positive-or-negative/positive-negative.rb
@@ -0,0 +1,10 @@
+puts "----------check the number is positive or negative"
+puts "Enter the number"
+a = gets.to_i
+
+
+if a>0
+    puts "It is Positive number"
+else
+    puts "It is negative number"
+end

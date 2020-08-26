3c895edac2e31e4194b14128e4d66cac4dbb2e95
karthykarthick <karthylock@gmail.com>
2020-08-27 00:58:14 +0530

    add checks the number is less or greater than 10

---
+++ Ruby-Programs/less-or-greater-than-10/less-or-great-10.rb
@@ -0,0 +1,8 @@
+puts "--------------check whether the number is greater or less than 10-----------------"
+puts "Enter the number to check"
+n = gets.to_i
+if n>10
+    puts "#{n} It is greater than 10"
+else
+    puts "#{n} It is less than 10"
+end
